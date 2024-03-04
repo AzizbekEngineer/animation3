e538377c42c11f919a2fce6caeea5e8a33a539fe
AzizbekEngineer <azizbekengineer@gmail.com>
2024-03-04 17:29:25 +0500

    pages tayyor

--- js/main.js
+++ js/main.js
@@ -1,41 +1,42 @@
-let navBar = document.getElementById('header__nav__hamburger--btn');
-let navList = document.getElementById('header__nav--list');
-let bactop = document.getElementById('bactop');
-let navShrink = document.getElementById('navshrink')
-let body = document.getElementById('body')
-let darkMode = document.getElementById("dark-mode");
-let darkModeIcon = document.getElementById("dark-mode__icon");
+let backTop = document.querySelector(".back-top");
+let header = document.querySelector(".header");
+let body = document.querySelector("body");
+let darkMode = document.querySelector(".header__nav__right__btns__dark-mode");
+let headerList = document.querySelector(".header__nav__right__list");
+let hamburger = document.querySelector(".header__nav__right__btns__hamburger");
+let closeHeaderList = document.querySelector(
+    ".header__nav__right__list__close"
+);
 
 
-window.addEventListener('scroll', function () {
-    showBactop()
-    showNavShrink()
-})
-
-navBar.addEventListener('click', function () {
-    navList.classList.toggle('show-header__nav-list')
-})
-
-darkMode.addEventListener("click", function () {
-    darkModeIcon.classList.toggle("fa-moon");
-    darkModeIcon.classList.toggle("fa-sun");
-    body.classList.toggle("light");
+window.addEventListener("scroll", function () {
+    showBackTop();
+    showHeaderShrink();
 });
 
-function showBactop() {
-    if (scrollY > 200) {
-        bactop.classList.add('show-bactop')
-    }
-    else {
-        bactop.classList.remove('show-bactop')
+darkMode.addEventListener("click", function () {
+    body.classList.toggle("dark");
+})
+
+hamburger.addEventListener("click", function () {
+    headerList.classList.toggle("show__header__nav__right__list");
+})
+
+closeHeaderList.addEventListener("click", function () {
+    headerList.classList.remove("show__header__nav__right__list");
+});
+
+function showBackTop() {
+    if (scrollY > 100) {
+        backTop.classList.add("show__back-top");
+    } else {
+        backTop.classList.remove("show__back-top");
     }
 }
-
-function showNavShrink() {
+function showHeaderShrink() {
     if (scrollY > 0) {
-        navShrink.classList.add('show-shrink')
-    }
-    else {
-        navShrink.classList.remove('show-shrink')
+        header.classList.add("show__header__shrink");
+    } else {
+        header.classList.remove("show__header__shrink");
     }
 }
